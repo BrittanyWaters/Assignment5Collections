941031fb1e38c6085782270083a908b4c40d78be
Sabrina <sabrina.sturteva@smail.astate.edu>
2019-04-02 15:42:44 -0500

    Initial Commit

---
+++ dataTable/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  dataTable
+//
+//  Created by Sabrina.Sturteva on 4/2/19.
+//  Copyright © 2019 Sabrina Sturtevant. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
